4efbfc15102e89c68f43c903b0ce8c7040479796
nc163 <kou1992@gmail.com>
2022-08-25 00:52:35 +0000

    refactor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./assets/css/index.css";
 import { reportWebVitals } from "./elements";
-import { Expenses, Invoices, Testpage } from "./routes";
-import App from "./App";
+import Routing from "./Routing";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="expenses" element={<Expenses />} />
-          <Route path="invoices" element={<Invoices />} />
-          <Route path="testpage" element={<Testpage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routing />
   </React.StrictMode>
 );
 
