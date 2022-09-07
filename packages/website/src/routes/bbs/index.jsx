b066359dd0963246f300aaf0bad5c3bf5b0d7cea
nc163 <kou1992@gmail.com>
2022-09-07 13:07:58 +0000

    update

--- packages/website/src/routes/bbs/index.jsx
+++ packages/website/src/routes/bbs/index.jsx
@@ -21,61 +21,62 @@ import useIpfs from "./hooks/use-ipfs";
 function Bbs() {
   const { ipfs, ipfsInitError } = useIpfsFactory({ commands: ["id"] });
   const id = useIpfs(ipfs, "id");
+  const [text, setText] = useState();
   const [version, setVersion] = useState(null);
 
   useEffect(() => {
     if (!ipfs) return;
-
     const getVersion = async () => {
       const nodeId = await ipfs.version();
       setVersion(nodeId);
     };
-
     getVersion();
   }, [ipfs]);
 
+  const _onClick = () => {
+    if (ipfs) {
+      const run = async () => {
+        let cid = await ipfs.dag.put({ text: text });
+        console.log(cid);
+        return cid;
+      };
+
+      run();
+    }
+  };
+
   return (
-    <div className="sans-serif">
-      <header className="flex items-center pa3 bg-navy bb bw3 b--aqua">
-        <h1 className="flex-auto ma0 tr f3 fw2 montserrat aqua">IPFS React</h1>
-      </header>
+    <div className="text-white">
       <main>
         {ipfsInitError && (
-          <div className="bg-red pa3 mw7 center mv3 white">
+          <div className="">
             Error: {ipfsInitError.message || ipfsInitError}
           </div>
         )}
         {(id || version) && (
-          <section className="bg-snow mw7 center mt5">
-            <h1 className="f3 fw4 ma0 pv3 aqua montserrat tc" data-test="title">
+          <section className="">
+            <h1 className="" data-test="title">
               Connected to IPFS
             </h1>
-            <div className="pa4">
+            <div className="">
               {id && <IpfsId obj={id} keys={["id", "agentVersion"]} />}
               {version && <IpfsId obj={version} keys={["version"]} />}
             </div>
           </section>
         )}
+        <p>文字を入力</p>
+        <input
+          style={{ color: "black" }}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={_onClick}>送信</button>
       </main>
-      <footer className="react-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="react-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </footer>
     </div>
   );
 }
 
 const Title = ({ children }) => {
-  return <h2 className="f5 ma0 pb2 aqua fw4 montserrat">{children}</h2>;
+  return <h2 className="">{children}</h2>;
 };
 
 const IpfsId = ({ keys, obj }) => {
@@ -85,7 +86,7 @@ const IpfsId = ({ keys, obj }) => {
       {keys?.map((key) => (
         <div className="mb4" key={key}>
           <Title>{key}</Title>
-          <div className="bg-white pa2 br2 truncate monospace" data-test={key}>
+          <div className="" data-test={key}>
             {obj[key].toString()}
           </div>
         </div>
